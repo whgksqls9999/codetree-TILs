5392ac097ad663047c3bf24023233705961524c0
whgksqls9999 <85666312+whgksqls9999@users.noreply.github.com>
2023-11-18 15:24:25 +0900

    Title: 두 개의 격자 비교하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231118/두 개의 격자 비교하기/compare-two-grid.js
@@ -0,0 +1,26 @@
+const input = require('fs').readFileSync(0).toString().split('\n');
+
+let [N, M] = input[0].split(' ').map((element) => parseInt(element));
+
+let arr1 = Array(N).fill(0).map(() => Array(M).fill(0));
+let arr2 = Array(N).fill(0).map(() => Array(M).fill(0));
+let ans = Array(N).fill(0).map(() => Array(M).fill(0));
+
+for(let i = 0; i < N; ++i){
+    for (let j = 0; j < M; ++j){
+        arr1[i][j] = input[i+1].split(' ')[j];
+    }
+}
+
+let result = '';
+
+for(let i = 0; i < N; ++i){
+    for (let j = 0; j < M; ++j){
+        if(input[i+5].split(' ')[j] != arr1[i][j]){
+            ans[i][j] = 1;
+        }
+    }
+    result += ans[i].join(' ') + '\n';
+}
+
+console.log(result);
